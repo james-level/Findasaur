3376d2ddc0464f59d71dd9a1abe99fcc7ae5e562
jah1603 <hendersonjamesalexander@gmail.com>
2018-12-17 13:47:57 +0000

    Horizontal pagination functional
    Co-authored-by: David Pears <DavidAPears@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import HeroImageCarousel from './components/HeroImageCarousel.js';
+import PaginatedHomepage from './components/PaginatedHomepage.js';
 import { StyleSheet, Text, View } from 'react-native';
 
 global.self = global;
@@ -9,7 +10,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <HeroImageCarousel/>
+        <PaginatedHomepage/>
       </View>
     );
   }
