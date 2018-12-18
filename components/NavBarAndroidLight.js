16aba634b27566d44362c6cc24dd464df4b20328
jah1603 <hendersonjamesalexander@gmail.com>
2018-12-18 15:40:19 +0000

    restyle and resize navbar
    Co-authored-by: David Pears <DavidAPears@users.noreply.github.com>

--- components/NavBarAndroidLight.js
+++ components/NavBarAndroidLight.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
 import NavBar, { NavGroup, NavButton, NavButtonText, NavTitle } from 'react-native-nav'
+import {StyleSheet} from 'react-native';
 
 export default class NavBarAndroidLight extends Component {
   render() {
     return (
-      <NavBar>
+      <NavBar style={styles}>
 
         <NavGroup>
           <NavButton onPress={this.props.home}>
-            <NavButtonText>
+            <NavButtonText style={styles.buttonText}>
               {"Main"}
             </NavButtonText>
             </NavButton>
           <NavButton onPress={() => alert('hi')}>
-            <NavButtonText>
-              {"Button"}
+            <NavButtonText style={styles.buttonText}>
+              {"Home"}
             </NavButtonText>
               </NavButton>
           <NavButton onPress={() => alert('hi')}>
-            <NavButtonText>
-              {"Button"}
+            <NavButtonText style={styles.buttonText}>
+              {"Contact"}
             </NavButtonText>
               </NavButton>
         </NavGroup>
@@ -27,3 +28,20 @@ export default class NavBarAndroidLight extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+ statusBar: {
+   backgroundColor: 'black',
+   height: 20
+ },
+ navBar: {
+   backgroundColor: 'black',
+   height: 40
+ },
+ title: {
+   color: '#fff',
+ },
+ buttonText: {
+   color: '#b5b5b5',
+ },
+})
