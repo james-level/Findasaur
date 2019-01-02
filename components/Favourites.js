c72f9a50c50008a3df46a059fc98f43757ae4f2e
jah1603 <hendersonjamesalexander@gmail.com>
2019-01-02 14:35:04 +0000

    add Favourites.js to store favourited dinosaurs

---
+++ components/Favourites.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import { Modal, View, Text, ScrollView, Linking, TouchableHighlight, Image } from 'react-native';
+import { Font, LinearGradient  } from 'expo';
+
+import InfoModalStyle from '../Stylesheets/InfoModalStyle.js';
+
+export default class Favourites extends Component {
+  render() {
+
+    retrieveFavourites = async () => {
+      try {
+        const value = await AsyncStorage.getItem('dinosaur');
+        if (value !== null) {
+          // We have data!!
+          console.log(value);
+          return value;
+        }
+       } catch (error) {
+         // Error retrieving data
+       }
+    }
+
+    return (
+      <View>
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.props.modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+          }}>
+          <View>
+
+        <LinearGradient
+        colors={['black', '#1e932d']}
+        style={{ padding: 25 }}>
+
+          <View style={InfoModalStyle.infoModal}>
+
+
+            </View>
+          </LinearGradient>
+        </View>
+      </Modal>
+
+    </View>
+    )
+  }
+}
