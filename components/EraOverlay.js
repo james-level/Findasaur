80eeb8c75a7be2e0d610f9d7557eb85f815169d2
jah1603 <hendersonjamesalexander@gmail.com>
2019-01-05 17:48:47 +0000

    era descriptions passed dynamically from ChooseTimePeriod to EraOverlay

--- components/EraOverlay.js
+++ components/EraOverlay.js
@@ -7,15 +7,16 @@ import EraOverlayStyle from '../Stylesheets/EraOverlayStyle.js';
 export default class EraOverlay extends Component {
   render() {
     return (
+      /* ANIMATION OPTIONS: fadeInUp, zoomIn, bounceIn, flipInX, lightSpeedIn */
       <Overlay visible={this.props.eraModalVisible} onClose={this.props.closeEraModal} closeOnTouchOutside
-      animationType="zoomIn" containerStyle={{backgroundColor: 'rgba(8, 10, 37, 0.78)'}}
+      animationType="fadeInUp" containerStyle={{backgroundColor: 'rgba(8, 10, 37, 0.78)'}}
       childrenWrapperStyle={{backgroundColor: '#00003f', borderRadius: 15}}
-      animationDuration={750}>
+      animationDuration={500}>
       {
         (hideModal, overlayState) => (
           <Fragment>
           <Text style={EraOverlayStyle.eraOverlayHeader}>{this.props.eraTitle}</Text>
-            <Text style={EraOverlayStyle.eraOverlayDescription}>The Late Cretaceous (100.5 to 66 million years ago) is the younger of two epochs into which the Cretaceous period is divided in the geologic timescale. Rock strata from this epoch form the Upper Cretaceous series. The Cretaceous is named after the white limestone known as chalk which occurs widely in northern France and is seen in the white cliffs of south-eastern England, and which dates from this time. </Text>
+            <Text style={EraOverlayStyle.eraOverlayDescription}>{this.props.eraDescription}</Text>
           </Fragment>
         )
       }
