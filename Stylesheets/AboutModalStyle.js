0485febaa7ad7d01bfe7884346a6cb61e21e7aa1
David A Pears <davidapears@gmail.com>
2019-01-10 12:50:19 +0000

    Adds 'about' modal to homne screen

---
+++ Stylesheets/AboutModalStyle.js
@@ -0,0 +1,65 @@
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+
+const AboutModalStyle = StyleSheet.create({
+  aboutModal:{
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  aboutModalHeading:{
+    color: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 50,
+    fontSize: 30,
+    backgroundColor: 'transparent',
+  },
+  aboutModalBody:{
+    color: 'black',
+    paddingTop: 50,
+    fontSize: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+  },
+  aboutModalText: {
+    fontSize: 17,
+    color: 'black',
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 10,
+  },
+  aboutModalHeader: {
+    fontSize: 22,
+    color: 'black',
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+
+  modalHeader: {
+    flex: 1,
+    justifyContent: 'center',
+    flexDirection: 'row',
+    padding: 0,
+    backgroundColor: 'indigo',
+    borderRadius: 5,
+    marginTop: 20
+  },
+
+  modalFavourite: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'white',
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+
+});
+
+export default AboutModalStyle;
