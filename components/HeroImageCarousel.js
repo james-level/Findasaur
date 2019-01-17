b2a42c199218f859cbd8d7843e9bcb2201dcd958
jah1603 <hendersonjamesalexander@gmail.com>
2019-01-17 14:06:44 +0000

    add two extra images to HeroImageCarousel.js
    Co-authored-by: David Pears <DavidAPears@users.noreply.github.com>

--- components/HeroImageCarousel.js
+++ components/HeroImageCarousel.js
@@ -4,13 +4,15 @@ import ImageSlider from 'react-native-image-slider';
 import HeroImageCarouselStyle from '../Stylesheets/HeroImageCarouselStyle.js';
 
   const images = [
-  'http://www.jurassicworld.com/sites/default/files/2018-06/960x540_0001_trex.png',
   'http://www.jurassicworld.com/sites/default/files/2018-06/960x540_0000_triceratops.png',
+  'http://www.jurassicworld.com/sites/default/files/2018-06/960x540_0001_trex.png',
   'http://www.jurassicworld.com/sites/default/files/2018-05/960x540_0009_carnotaurus.png',
   'http://www.jurassicworld.com/sites/default/files/2018-06/960x540_0006_mosasaurus_0.png',
+  'http://www.jurassicworld.com/sites/default/files/2018-06/960x540_0008_gallimimus_0.png',
   'http://www.jurassicworld.com/sites/default/files/2018-06/960x540_0015_ankylosaurus.png',
   'http://www.jurassicworld.com/sites/default/files/2018-05/960x540_0002_stygimoloch.png',
   'http://www.jurassicworld.com/sites/default/files/2018-05/960x540_0011_blue.png',
+  'http://www.jurassicworld.com/sites/default/files/2018-06/960x540_0003_stegosaurus_0.png',
   'http://www.jurassicworld.com/sites/default/files/2018-06/960x540_0004_pteranodon_0.png',
   ];
 
